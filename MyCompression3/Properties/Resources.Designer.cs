dcea3ad7f6ccd2bfb138a453df0883e1514fb6ba
johnson91047 <jj91047@gmail.com>
2018-10-15 22:25:07 +0800

    modify project file

--- MyCompression3/Properties/Resources.Designer.cs
+++ MyCompression3/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace MyCompression.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyCompression3.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MyCompression.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
